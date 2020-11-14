9d1ba657ed1a960973d008f0d8e1531d79d5d30e
nojmar <73884817+nojmar@users.noreply.github.com>
2020-11-14 17:51:10 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+     unsigned int licznik = 0;
+  unsigned int liczbaZaglowcow = 0;
+  Stocznia stocznia{};
+
+  while (licznik<towar) {
+
+    Statek* s1 = stocznia();
+    unsigned int iloscTowaru = s1->transportuj();
+    licznik = licznik + iloscTowaru;
+    
+    if (Zaglowiec* z = dynamic_cast<Zaglowiec*>(s1)) {
+     liczbaZaglowcow = liczbaZaglowcow + 1;
+    }
+
+    delete s1;
+  }
+
+  return liczbaZaglowcow;
+    
 }
